9cc73c75a955b879795e3c8b592437d38082d22f
Francisco Miguel Suero Nin <franciscosueronin@gmail.com>
2017-02-19 17:59:32 -0400

    server and client -Synchronous/ Asynchronous

---
+++ src/ClientsLibraries/main.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClientsLibraries
+{
+    class main
+    {
+        public static int Main(String[] args)
+        {
+            ClientAsync cliAsyc = new ClientAsync();
+            ClientSync cliSync = new ClientSync();
+
+            //show client Async
+            //cliAsyc.StartClientAsync("localhost", 8082);
+
+            //show client sync
+            cliSync.StartClientSync("localhost", 8082);
+
+            Console.WriteLine("\n press any key to exit...");
+            Console.ReadKey();
+
+            return 0;
+        }
+    }
+}
